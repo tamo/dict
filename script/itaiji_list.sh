6fe0b910c598dad07756bee6afa606b08cdfb3f8
Tamotsu Takahashi <ttakah+github@gmail.com>
2025-03-14 11:53:17 +0900

    script: github actions が失敗するので調整

--- script/itaiji_list.sh
+++ script/itaiji_list.sh
@@ -6,9 +6,14 @@ sed -e 's,<TRclass=.><TD>[0-9]*</TD><TD>\(.\)</TD><TD>\([^&]*\)&nbsp\;</TD></TR>
 sed -e '1d;/^A/,$d' | \
 while read line; do
   for i in $(seq 1 ${#line}); do
-    char=$(echo "$line" | cut -c "$i")
-    others=$(echo "$line" | sed "s/$char//")
-    printf "%s /%s\\n" "$char" "$(echo "$others" | sed 's/./&\//g')"
+    char="$(echo $line | cut -c $i)"
+    others="${line:0:$(($i-1))}${line:$i}"
+
+    output="$char /"
+    for j in $(seq 1 ${#others}); do
+      output="$output$(echo $others | cut -c $j)/"
+    done
+    echo "$output"
   done
 done
 
